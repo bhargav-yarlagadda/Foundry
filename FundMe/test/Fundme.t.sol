b53f811a01e85e9a3caa7a559269b72efc4fd2e1
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2025-04-20 10:00:59 +0530

    feat.deploying contracts via tests

---
+++ FundMe/test/Fundme.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {Test} from "forge-std/Test.sol";
+contract FundMetest is Test {
+    uint256 number = 256;
+    function setUp() external {
+        number = 100;
+    }
+    function testDemo() public {
+        
+    }
+}
